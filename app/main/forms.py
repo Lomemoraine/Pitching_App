5d120e3feb7561bc3f28107976af85113deaf4b6
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-05-09 23:01:29 +0300

    add form to enable user submit a new pitch

---
+++ app/main/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import InputRequired
+
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title', validators=[InputRequired()])
+    post = TextAreaField('Pitch Message', validators=[InputRequired()])
+    category = SelectField('Pitch Category', choices=[('One Word Pitch', 'One Word Pitch'), ('Question Pitch', 'Question Pitch'), ('Rhymic Pitch', 'Rhymic Pitch'), ('Twitter Pitch', 'Twitter Pitch'),('Tech Pitch', 'Tech Pitch')],
+                           validators=[InputRequired()])
+    submit = SubmitField('Post')
--- app/main/views.py
+++ app/main/views.py
@@ -8,4 +8,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = 'Pitch Raine'
+    
+    
+    return render_template('index.html' ,title=title)
