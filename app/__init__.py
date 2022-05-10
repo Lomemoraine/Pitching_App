6ea4609ec96439350580f1a47cdba2f788b655e9
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-05-10 12:53:58 +0300

    update welcome mail messages for users who register into our application

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config_options
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 # Instanciating Flask Extensions
 bootstrap = Bootstrap()
@@ -10,6 +12,8 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 
 
@@ -17,10 +21,14 @@ def create_app(config_name):
     app = Flask(__name__)
      # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    
+    # configure UploadSet
+    configure_uploads(app,photos)
       # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     
     
--- config.py
+++ config.py
@@ -2,6 +2,14 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET-_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorraine:gift1234@localhost/pitching'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+     #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     pass
     
 class ProdConfig(Config):
