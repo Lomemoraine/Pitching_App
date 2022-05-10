1bd7372c45c7d9e3f21dfc28856a8bc5632596ac
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-05-10 20:43:03 +0300

    add update profile pic path in main views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from .forms import PitchForm,UpdateProfile
-from .. import db
+from .. import db,photos
 
 from ..models import Pitch,User
 from flask_login import login_required
@@ -51,3 +51,14 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
