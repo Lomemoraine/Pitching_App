db1b015ae9df5a0a71cde50c0e7cbf396369198d
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-05-10 09:42:42 +0300

    render the login form in views.py and update the navigation bar

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,24 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request # flash helps to display error messages to the user
 from . import auth
+from flask_login import login_user
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
 
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Check your credentials')
+
+    title = "Pitches login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
