9a145bd12b133a1ce999097b6007e99db72e3683
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-05-11 12:58:52 +0300

    manually drop a column i didnt need in my pitches table and do a migration

--- app/models.py
+++ app/models.py
@@ -56,7 +56,6 @@ class Pitch(db.Model):
     __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.String, nullable=False)
     pitch = db.Column(db.String, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     comment = db.relationship('Comment', backref='pitch', lazy='dynamic')
